04843ccaaaaef7116c0aea11fa91032b275421bb
Aaron Chafetz <achafetz@usaid.gov>
2021-05-11 11:45:46 -0400

    setup project

---
+++ Scripts/90_initiate-proj.R
@@ -0,0 +1,16 @@
+# PROJECT:  agitprop
+# AUTHOR:   A.Chafetz | USAID
+# PURPOSE:  project setup
+# LICENSE:  MIT
+# DATE:     2021-05-11
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  library(glamr)
+
+
+# INITIATE PROJECT --------------------------------------------------------
+
+  # si_setup()
+  folder_setup()
