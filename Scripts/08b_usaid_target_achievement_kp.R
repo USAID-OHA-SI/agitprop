6de240d3952b848f921a36629bc6f17c527c27b1
Aaron Chafetz <achafetz@usaid.gov>
2021-05-26 16:50:21 -0400

    convert achv box plots to waffle charts

--- Scripts/08b_usaid_target_achievement_kp.R
+++ Scripts/08b_usaid_target_achievement_kp.R
@@ -17,6 +17,7 @@
   library(patchwork)
   library(ggtext)
   library(glue)
+  library(waffle) #devtools::install_github("hrbrmstr/waffle")
   
   source("Scripts/99_utilities.R")
 
@@ -55,42 +56,58 @@
     ungroup() %>% 
     calc_achv(curr_qtr)
   
-  df_viz <- df_achv %>% 
-    mutate(x = .5, 
-           y = .5,
-           goal = ifelse(indicator == "TX_CURR", 1, trgt_rng),
+
+  df_viz <- df_achv %>%
+    mutate(achv_round = round(achievement*100),
+           achv_round = ifelse(achv_round > 100, 100, achv_round),
+           gap = 100-achv_round) %>% 
+    pivot_longer(c(achv_round, gap), names_to = "status") %>% 
+    mutate(achv_color = ifelse(status == "gap", "#EBEBEB", achv_color),
+           achv_color = ifelse(achv_color == trolley_grey_light, trolley_grey, achv_color),
+           achv_alpha = ifelse(status == "gap", .1, 1),
            indicator = factor(indicator, ind_sel),
-           lab = ifelse(indicator == ind_sel[1],
-                        glue("Results - {clean_number(cumulative, 1)}\nTargets - {clean_number(targets,1)}"),
-                        glue("{clean_number(cumulative, 1)}\n{clean_number(targets,1)}"))) 
+           ind_lab = case_when(indicator == "PrEP_NEW" ~ "Newly enrolled on antiretroviral pre-exposure prophylaxis",
+                               indicator == "KP_PREV" ~ "Key pop individuals provided prevention services",
+                               indicator == "HTS_TST" ~ "Receiving HIV testing service and results",
+                               indicator == "HTS_TST_POS" ~ "Receiving HIV testing services and positive results",
+                               indicator == "TX_NEW" ~ "Newly enrolled on antiretroviral therapy",
+                               indicator == "TX_CURR"~ "Currently receiving antiretroviral therapy"),
+           ind_lab = str_wrap(ind_lab, width = 25),
+           val_lab = ifelse(indicator == ind_sel[1],
+                            glue("Results - {clean_number(cumulative)}\nTargets - {clean_number(targets)}"),
+                            glue("{clean_number(cumulative)}\n{clean_number(targets)}")),
+           full_lab = glue("{ind_lab}\n\n{val_lab}")) %>% 
+    arrange(indicator) %>% 
+    mutate(full_lab = fct_inorder(full_lab))
+  
   
   df_viz %>% 
-    ggplot(aes(x, y)) +
-    geom_point(aes(size = achievement, color = achv_color), shape = 15, alpha = .3) +
-    geom_point(aes(size = goal), shape = 0, stroke = 1.3, color = trolley_grey) +
-    geom_text(aes(label = percent(achievement, 1)),
-              family = "Source Sans Pro SemiBold", color = "gray30") +
-    geom_text(aes(y = .1, label = lab),
-              family = "Source Sans Pro", color = trolley_grey) +
-    facet_grid(~indicator) +
-    scale_size(range = c(10, 40)) +
+    ggplot(aes(fill = achv_color, values = value, alpha = achv_alpha)) +
+    geom_waffle(color = "white", size = 1, n_rows = 10, flip = TRUE) +
+    geom_text(aes(x = 5, y  = 12, label = percent(achievement, 1), color = achv_color),
+              family = "Source Sans Pro SemiBold", size = 14/.pt) +
+    facet_wrap(~full_lab, nrow = 1, strip.position = "bottom") +
+    expand_limits(y = 14) +
+    scale_x_discrete() +
+    scale_y_continuous(labels = function(x) x * 10, # make this multiplyer the same as n_rows
+                       expand = c(0,0)) +
+    scale_fill_identity() +
     scale_color_identity() +
-    si_style_nolines() +
-    expand_limits(x = c(0, 1), y = c(0, .8)) +
-    labs(x = NULL, y = NULL,
+    scale_alpha_identity() +
+    coord_equal() +
+    labs(x= NULL, y = NULL,
          title = "SELECT USAID KEY POPULATION INDICATOR PERFORMANCE ACROSS ALL COUNTRIES",
          subtitle = glue("as of {curr_pd}, goal of being at around {percent(trgt_rng)} of the FY target"),
          caption = glue("Source: {msd_source}
                         SI analytics: {paste(authors, collapse = '/')}
                      US Agency for International Development")) +
-    theme(legend.position = "none",
-          strip.text.x = element_text(hjust = .5, size = 14),
-          axis.text.x = element_blank(),
-          axis.text.y = element_blank())
+    si_style_nolines() +
+    theme(axis.text.y = element_blank(),
+          strip.text.x = element_text(hjust = .5),
+          panel.spacing = unit(1, "pt")) +
+    guides(fill = guide_legend(reverse = TRUE))
   
   si_save("Images/08b_achievement_kp.png", height = 4)
   
-  ggsave("Graphics/08b_achievement_kp.svg", height = 4, width = 10)
-  
-  
+  si_save("Graphics/08b_achievement_kp.svg", height = 4)
   
