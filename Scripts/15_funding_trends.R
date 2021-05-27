6a6c9a7cc842579048b064fa467cb97fc9ed5daf
Aaron Chafetz <achafetz@usaid.gov>
2021-05-27 13:47:33 -0400

    remove commodities and adj budget source

--- Scripts/15_funding_trends.R
+++ Scripts/15_funding_trends.R
@@ -3,7 +3,7 @@
 # PURPOSE:  funding trends highlights
 # LICENSE:  MIT
 # DATE:     2021-05-25
-# UPDATED: 
+# UPDATED:  2021-05-27
 
 # DEPENDENCIES ------------------------------------------------------------
   
@@ -17,6 +17,7 @@
   library(patchwork)
   library(ggtext)
   library(glue)
+  library(janitor)
   
 
   source("Scripts/99_utilities.R")
@@ -25,13 +26,13 @@
   
   authors <- c("Aaron Chafetz", "Tim Essam")
 
-  fsd_source <- "FSD COP17-20 [2021-05-14]"
+  #data.pepfar.gov > Additional Data > Public Partner Budget Dataset
+  budget_source <- "PEPFAR Spotlight Public Partner Budget 2018-2021 [2021-05-27]"
     
 # IMPORT ------------------------------------------------------------------
   
-  df_fsd <- si_path() %>% 
-    return_latest("Financial_Structured_Dataset_COP17") %>% 
-    read_msd()   
+  df_budget <- read_tsv("Data/Public_Partner_Budget_2018-2021.txt") %>% 
+    clean_names()
   
   df_msd <- si_path() %>% 
     return_latest("OU_IM_FY19") %>% 
@@ -51,28 +52,40 @@
     identifypd() %>% 
     msd_period(period = .)
   
-  #remove M&O & keep PEPFAR and USAID
-  df_fsd <- df_fsd %>% 
-    bind_rows(df_fsd %>% mutate(fundingagency = "PEPFAR")) %>% 
+  #identify supply chain mechanism to remove
+  df_sch_mechs <- df_budget %>% 
+    filter(str_detect(mechanism_name, "GHSC|Supply Chain")) %>% 
+    distinct(mechanism_id, mechanism_name) %>% 
+    filter(str_detect(mechanism_name, "Kenya Supply Chain System Strengthening|TA|QA", negate = TRUE))
+  
+  #remove M&O + supply chain & keep PEPFAR and USAID
+  df_budget <- df_budget %>% 
+    bind_rows(df_budget %>% mutate(funding_agency = "PEPFAR")) %>% 
     filter(record_type != "Management and Operations",
-           fundingagency %in% c("USAID", "PEPFAR"))
+          !mechanism_id %in% unique(df_sch_mechs$mechanism_id),
+           funding_agency %in% c("USAID", "PEPFAR"))
   
   #agg fiscal year totals
-  df_cop_tot <- df_fsd %>% 
-    group_by(fiscal_year,fundingagency) %>% 
-    summarise(across(cop_budget_total, sum, na.rm = TRUE)) %>% 
+  df_cop_tot <- df_budget %>% 
+    group_by(implementation_year,funding_agency) %>% 
+    summarise(across(total_planned_funding, sum, na.rm = TRUE)) %>% 
     ungroup() %>% 
-    group_by(fiscal_year) %>% 
-    mutate(share = case_when(fundingagency == "USAID" ~ cop_budget_total/lag(cop_budget_total)),
-           total = case_when(fundingagency == "PEPFAR" ~ cop_budget_total)) %>% 
+    group_by(implementation_year) %>% 
+    mutate(share = case_when(funding_agency == "USAID" ~ total_planned_funding/lag(total_planned_funding)),
+           total = case_when(funding_agency == "PEPFAR" ~ total_planned_funding)) %>% 
     ungroup()
   
   #C&T only budget
-  df_ct <- df_fsd %>% 
-    filter(program == "C&T",
-           fundingagency == "USAID") %>% 
-    count(fiscal_year, program, wt = cop_budget_total, name = "cop_budget_total")
-
+  df_ct <- df_budget %>% 
+    filter(funding_agency == "USAID") %>% 
+    select(implementation_year, funding_agency, circ:hvtb) %>% 
+    mutate(across(circ:hvtb, na_if, 0)) %>% 
+    pivot_longer(-c(implementation_year, funding_agency),
+                 names_to = "budget_code",
+                 values_drop_na = TRUE) %>% 
+    filter(budget_code %in% c("hbhc", "htxs", "pdcs", "pdtx", "hvtb", "htxd")) %>% 
+    count(implementation_year, funding_agency, wt = value, name = "budget")
+    
   #Treatment data to combine with C&T budget
   df_tx <- df_msd %>% 
     bind_rows(df_arch) %>% 
@@ -102,8 +115,8 @@
 
   #USAID Total budget out of PEPFAR
   df_cop_tot %>% 
-    ggplot(aes(fiscal_year, cop_budget_total, fill = fundingagency)) + 
-    geom_col(aes(fill = fundingagency, alpha = fundingagency), position = "identity") +
+    ggplot(aes(implementation_year, total_planned_funding, fill = funding_agency)) + 
+    geom_col(aes(fill = funding_agency, alpha = funding_agency), position = "identity") +
     geom_text(aes(label = percent(share, 1)), na.rm = TRUE, vjust = -1, 
               family = "Source Sans Pro SemiBold", color = trolley_grey) +
     geom_errorbar(aes(ymin = total, ymax = total), color = trolley_grey_light, 
@@ -115,9 +128,8 @@
                        position = "right", expand = c(.005, .005)) +
     labs(x = NULL, y = NULL, fill = NULL, alpha = NULL,
          title = "USAID'S BUDGET AND SHARE OF PEPFAR BUDGET HAS REMAINED CONSISTENT OVER THE LAST FEW YEARS",
-         subtitle = "Annual Country Operation Plan (COP) budgets including commodities",
-         caption = glue("Note: M&O excluded
-                        Source: {fsd_source}
+         subtitle = "Annual Country Operation Plan (COP) budgets excluding commodities and M&O",
+         caption = glue("Source: {budget_source}
                         SI analytics: {paste(authors, collapse = '/')}
                      US Agency for International Development")) +
     si_style_ygrid()
@@ -129,14 +141,14 @@
   
   #C&T budget trends
   v1 <- df_ct %>% 
-    ggplot(aes(fiscal_year, cop_budget_total)) +
-    geom_blank(aes(y = cop_budget_total *1.01)) +
+    ggplot(aes(implementation_year, budget)) +
+    geom_blank(aes(y = budget *1.01)) +
     geom_col(fill = burnt_sienna) +
     geom_hline(yintercept = 0, color = trolley_grey) +
-    geom_text(aes(label = clean_number(cop_budget_total, 2)),
+    geom_text(aes(label = clean_number(budget, 2)),
               vjust = -.9, color = burnt_sienna, family = "Source Sans Pro SemiBold") +
     labs(x = NULL, y = NULL,
-         subtitle = "Flat Care and Treatment Budgets") +
+         subtitle = "Relatively t Care and Treatment Budgets (billions, USD)") +
     si_style_nolines() +
     theme(axis.text.y = element_blank())
   
@@ -163,7 +175,7 @@
   #combine
   v1 + v2 + plot_annotation(
     title = "USAID PROVIDING FOR MORE PATIENTS EACH YEAR DESPITE A FLAT BUDGET FOR CARE AND TREATMENT",
-    caption = glue("Source: {msd_source} + {fsd_source}
+    caption = glue("Source: {msd_source} + {budget_source}
                       SI analytics: {paste(authors, collapse = '/')}
                      US Agency for International Development")
   ) & 
