3ec849777557892493d14f3bad3d8be9cf4e2f72
Aaron Chafetz <achafetz@usaid.gov>
2021-06-30 13:39:56 -0400

    update to work with MMD

--- Scripts/17b_stringency_mer.R
+++ Scripts/17b_stringency_mer.R
@@ -23,6 +23,7 @@
   library(COVIDutilities)
   library(jsonlite)
   library(zoo)
+  library(usethis)
   
   source("Scripts/99_utilities.R")
 
@@ -167,10 +168,13 @@
 
   #select indicator and reshape long
   df_mer <- df %>% 
-    filter(indicator %in% c("HTS_TST", "TX_NEW", "TX_CURR", "TX_PVLS"),
-           standardizeddisaggregate %in% c("Total Numerator", "Total Denominator"),
+    filter(indicator %in% c("HTS_TST", "HTS_TST_POS","TX_NEW", "TX_CURR", "TX_PVLS", "VMMC_CIRC"),
+           standardizeddisaggregate %in% c("Total Numerator", "Total Denominator", "Age/Sex/ARVDispense/HIVStatus"),
+           otherdisaggregate %in% c(NA, "ARV Dispensing Quantity - 3 to 5 months", "ARV Dispensing Quantity - 6 or more months"),
            fiscal_year >= 2019,
            mech_code != "16772") %>% 
+    mutate(indicator = ifelse(standardizeddisaggregate == "Age/Sex/ARVDispense/HIVStatus", "TX_MMD_o3mo", indicator)) %>% 
+    clean_indicator() %>% 
     group_by(fiscal_year, indicator, countryname) %>% 
     summarise(across(starts_with("qtr"), sum, na.rm = TRUE), .groups = "drop") %>% 
     reshape_msd() %>% 
@@ -200,13 +204,15 @@
   df_viz <- df_stringency_viz %>% 
     bind_rows(df_early) %>%
     tidylog::full_join(df_mer %>%
-                         select(-latest) %>%
                          pivot_wider(names_from = indicator)) %>% 
     mutate(countryname = recode(countryname, "Democratic Republic of the Congo" = "DRC"))
     
+  df_viz <- df_viz %>% 
+    mutate(VLS = TX_PVLS/TX_PVLS_D,
+           TX_MMD_o3mo_share = TX_MMD_o3mo/TX_CURR)
     
   df_viz <- df_viz %>% 
-    pivot_longer(matches("HTS|TX"),
+    pivot_longer(-date:-period,
                  names_to = "indicator")
 
   df_dates <- df_mer %>% 
@@ -215,21 +221,37 @@
     mutate(fy = str_sub(period, end = -3)) %>% 
     filter(str_detect(period, "Q1"))
   
-  plot_mer_stringency <- function(ind_sel, n_countries = 16){
+  lst_mmd_order <- df_viz %>% 
+    filter(date == max(date),
+           indicator == "TX_CURR",
+           !countryname %in% c("South Africa", "Namibia")) %>% 
+    arrange(desc(value)) %>% 
+    pull(countryname)
+  
+  plot_mer_stringency <- function(ind_sel, n_countries = 16, save = FALSE){
+    
+    if(str_detect({ind_sel}, "MMD")){
+      df_viz <-  filter(df_viz, date >= "2020-01-01")
+      df_dates <- filter(df_dates, date >= "2020-01-01")
+    }
     
     df_viz <- filter(df_viz, indicator == {ind_sel})
     
+      
     #latest value for ordering
     lst_lrg <- df_viz %>% 
       filter(date == max(date)) %>% 
       slice_max(order_by = value, n = n_countries) %>% 
       pull(countryname)
     
+    if(str_detect({ind_sel}, "MMD"))
+      lst_lrg <- lst_mmd_order[1:n_countries]
+    
     df_viz <- df_viz %>% 
       filter(countryname %in% lst_lrg) %>% 
       mutate(countryname = factor(countryname, lst_lrg)) 
     
-    df_viz %>% 
+    v <- df_viz %>% 
       ggplot(aes(date, value), na.rm = TRUE) +
       geom_area(alpha = .4, color = genoa, fill = genoa_light, na.rm = TRUE) +
       geom_vline(data = filter(df_viz, tenth_case ==1, countryname %in% lst_lrg),
@@ -240,9 +262,10 @@
       # scale_y_continuous(labels = number_format(accuracy = y_accuracy, scale = y_scale, suffix = y_suffix, big.mark = ",")) +
       scale_y_dynamic() +
       scale_x_date(breaks = as.Date(df_dates$date), labels = df_dates$fy) +
+      expand_limits(y = 1) +
       scale_color_identity() +
       labs(x = NULL, y = NULL,
-           title = "Comparing PEPFAR MER trends aginst the strictness of ‘lockdown style’ policies" %>% toupper,
+           title = "Comparing PEPFAR MER trends against the strictness of ‘lockdown style’ policies" %>% toupper,
            subtitle = glue("{ind_sel} in the largest {n_countries} countries | {min(df_dates$fy)}-{max(df_dates$fy)}"),
            caption = glue("Sources: PEPFAR {msd_source}, JHU COVID-19 feed, Stringency Index from Blavatnik School of Government at Oxford University
          SI analytics: SI analytics: {paste(authors, collapse = '/')}
@@ -250,11 +273,20 @@
       si_style_ygrid() +
       theme(panel.spacing.x = unit(.5, "lines"),
             panel.spacing.y = unit(.5, "lines"))
+    
+    if(save == TRUE){
+      file_out <- glue("Graphics/17b_covid_{ind_sel}_trends.svg")
+      usethis::ui_info("saving to {ui_field(file_out)}")
+      si_save(file_out, plot = v)
+    }
+    
+    return(v)
   }
   
-  plot_mer_stringency("TX_NEW")
-  plot_mer_stringency("HTS_TST", y_accuracy = .01, y_scale = 1e-6, y_suffix = "m")
+  plot_mer_stringency("TX_NEW", save = TRUE)
+  plot_mer_stringency("TX_CURR", save = TRUE)
+  plot_mer_stringency("HTS_TST_POS", save = TRUE)
+  plot_mer_stringency("HTS_TST", save = TRUE)
+  plot_mer_stringency("TX_MMD_o3mo_share", n_countries = 15, save = TRUE)
   
-  
-
   
