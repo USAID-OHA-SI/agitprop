4602c7bea06dbf423baf2fe400e671b14c5ea0ca
Petrovic <NPETROVIC@USAID.GOV>
2022-08-08 16:14:34 -0400

    Remove doublecounting & simplify calculation

--- Scripts/gpm_mmd_tripsaverted_calculation_quarterly.R
+++ Scripts/gpm_mmd_tripsaverted_calculation_quarterly.R
@@ -36,7 +36,6 @@ df %>%
   view()
   
         
-
 #keep just TX_CURR/MMD and reshape
 df_mmd <- df %>% 
   filter(funding_agency == "USAID",
@@ -53,72 +52,43 @@ df_mmd <- df %>%
   reshape_msd() %>% 
   filter(value > 0)
 
-#create group for o3mo and o6mo via reshaping for plotting
-df_mmd <- df_mmd %>% 
+#create group for mmd3mo and mmd6mo 
+df_mmd_agency <- df_mmd %>% 
   mutate(country = recode(country,
                               "Democratic Republic of the Congo" = "DRC",
                               "Dominican Republic" = "DR")) %>% 
   select(-period_type) %>% 
   pivot_wider(names_from = otherdisaggregate) %>% 
-  rowwise() %>% 
-  mutate(#unknown = total - sum(`Less than 3 months`, `3 to 5 months`, `6 or more months`, na.rm = TRUE),
-    #unknown = ifelse(unknown < 0, 0, unknown),
-    o3mmd = sum(`3 to 5 months`, `6 or more months`, na.rm = TRUE)
-  ) %>%
-  ungroup() %>% 
-  rename(o6mmd = `6 or more months`) %>% 
-  select(-`Less than 3 months`, -`3 to 5 months`) %>% 
-  pivot_longer(-c(period, country, indicator, total), 
-               names_to = "otherdisaggregate",
-               values_to = "tx_mmd") %>% 
-  rename(tx_curr = total) 
-
-#aggregate up to USAID-wide level
-df_mmd_agency <- df_mmd %>% 
-  #mutate(otherdisaggregate = recode(otherdisaggregate,
-  #                                  "o3mmd" = "MMD - 3 months or more",
-  #                                  "o6mmd" = "MMD - 6 months or more")) %>% 
-  group_by(period, otherdisaggregate) %>% 
-  summarise(across(c(tx_curr, tx_mmd), sum,na.rm = TRUE)) %>% 
-  ungroup() %>%
-  mutate(share = tx_mmd / tx_curr)
-
-#CALCULATION ----------------------------------------------------------------
-
-df_mmd_agency_wide<-df_mmd_agency %>%
-  select(-share) %>%
-  pivot_wider(values_from=tx_mmd, names_from=otherdisaggregate) %>%
+  rename(mmd3mo = `3 to 5 months`) %>%
+  rename(mmd6mo = `6 or more months`) %>% 
+  #rename(nommd= `Less than 3 months`) %>%
+  #rowwise() %>%
+  #mutate(total_sum=sum(mmd3mo,mmd6mo,nommd, na.rm=TRUE))
+  #used to check whether the categories add up to the total -- doesnt seem to
+  #be the case -- they are close but there is some missing data
+  select(-`Less than 3 months`) %>%
+  group_by(period) %>%
+  summarise(across(c(mmd3mo, mmd6mo), sum, na.rm = TRUE)) %>%
+  ungroup %>%
+  #Calculation starting at COVID ramp-up FY20Q3
   filter(!(period %in% c("FY20Q1","FY20Q2")))
 
-## This code duplicates the calculation from before using a quarter by quarter estimate
-## Matches KS estimate
-    ##Since the COVID-lockdowns (FY20Q3), we have helped to save beneficiaries up to 
-    ##79 million clinic trips to pick up their treatment due to multi-month dispensing 
-    ##(as of FY22Q2).
+#CALCULATION ---------------------------------------------------------------
+
+## Calculates trips averted using quarter by quarter estimates
 
 trips_averted_mmd=0
 
-for (i in 1:nrow(df_mmd_agency_wide)){
+for (i in 1:nrow(df_mmd_agency)){
   # For all those on 3 month MMD 2 trips per quarter are averted
   # For those on 6 month MMD either 2 or 3 trips per month are averted so 2.5 on average
   # Note that we are not modeling possibility that patient starts on MMD at the end of the quarter so this 
-  # is an upper bound
-  trips_averted_mmd<- trips_averted_mmd+2*df_mmd_agency_wide$o3mmd[i] + 2.5*df_mmd_agency_wide$o6mmd[i]
+  # is an upper bound. However, 3 month mmd refers to "3-5mo" so there is an underestimate of 
+  # trips averted in this case. 
+  trips_averted_mmd<- trips_averted_mmd+2*df_mmd_agency$mmd3mo[i] + 2.5*df_mmd_agency$mmd6mo[i]
 }
 
 print(trips_averted_mmd)
-
-## However, this may be the more accurate calculation since the previous is double counting 6 mo MMD -- 
-## it is included in both 3mo + over & 6 mo. 
-
-trips_averted_mmd=0
-for (i in 1:nrow(df_mmd_agency_wide)){
-  # Subtract the 6mo total from 3mo to avoid double-counting
-  trips_averted_mmd<- trips_averted_mmd+2*(df_mmd_agency_wide$o3mmd[i]-df_mmd_agency_wide$o6mmd[i]) + 
-                      2.5*df_mmd_agency_wide$o6mmd[i]
-}
-print(trips_averted_mmd)
-
 
 
 
