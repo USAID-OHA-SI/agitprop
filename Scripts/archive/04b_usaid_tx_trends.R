ee983c400dac4523650b7659b4243ecd2952aa75
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-09 15:08:34 -0400

    add ref id and adapt for fy22

--- Scripts/archive/04b_usaid_tx_trends.R
+++ Scripts/archive/04b_usaid_tx_trends.R
@@ -1,6 +1,7 @@
 # PROJECT:  agiprop
 # AUTHOR:   A.Chafetz | USAID
 # PURPOSE:  treatment scale up since PEPFAR start
+# REF ID:   36402aea 
 # LICENSE:  MIT
 # DATE:     2021-05-14
 # UPDATED:  2021-10-07
@@ -20,18 +21,20 @@
   library(janitor)
   library(lubridate)
 
-  source("Scripts/99_utilities.R")
+  source("Scripts/archive/99_utilities.R")
 
 # GLOBAL VARIABLES --------------------------------------------------------
   
-  authors <- c("Aaron Chafetz", "Tim Essam")
+  authors <- c("Aaron Chafetz", "Tim Essam", "Karishma Srikanth")
+  
+  ref_id <- "36402aea"
   
 
 # IMPORT ------------------------------------------------------------------
   
   #Current MSD
   df <- si_path() %>% 
-    return_latest("OU_IM_FY19") %>% 
+    return_latest("OU_IM_FY20") %>% 
     read_msd()
   
   #Archived MSD
@@ -48,11 +51,11 @@
   
   df_tx <- df %>% 
     bind_rows(df_arch) %>% 
-    filter(fundingagency == "USAID",
+    filter(funding_agency == "USAID",
            indicator %in% c("TX_CURR", "TX_NEW"),
            standardizeddisaggregate == "Total Numerator",
            fiscal_year <= curr_fy) %>% 
-    group_by(fiscal_year, fundingagency, indicator) %>% 
+    group_by(fiscal_year, funding_agency, indicator) %>% 
     summarise(across(cumulative, sum, na.rm = TRUE)) %>% 
     ungroup()
   
@@ -79,7 +82,15 @@
            yrs = year - lag(year)) %>% 
     filter(year == max(year))
   
-  df_tx %>% 
+  #data point for context
+  curr_val <-  df_tx %>% 
+    filter(period == max(period),
+           indicator == "TX_CURR") %>%
+    select(indicator, value) %>% 
+    mutate(value = clean_number(value, 1)) %>% 
+    pull(value)
+  
+df_tx %>% 
     ggplot(aes(year, value)) +
     geom_col(aes(alpha = bar_alpha, fill = ind_label),
              position = "identity") +
@@ -88,21 +99,24 @@
                        position = "right", expand = c(.005, .005)) +
     scale_x_continuous(expand = c(.005, .005),
                        n.breaks = unique(df_tx$period) %>% length())+
+    geom_text(aes(label = clean_number(value, 1), vjust = -0.3,
+                  family = "Source Sans Pro")) +
+    expand_limits(y = 72e5) +
     scale_fill_manual(values = c(genoa_light, genoa)) +
     scale_alpha_identity() +
     labs(x = NULL, y = NULL, fill = NULL,
          title = glue("USAID HAS ADDED {title_info$added} PATIENTS ONTO TREATMENT IN THE PAST {title_info$yrs} YEARS"),
+         subtitle = glue("As of {curr_pd}, USAID provides treatment for {curr_val} patients"),
          caption = glue("Source: {msd_source} (including FY15-18)
-                        SI analytics: {paste(authors, collapse = '/')}
-                     US Agency for International Development")) +
+                        SI analytics: {paste(authors, collapse = '/')} | Ref ID: {ref_id}")) +
     si_style_nolines()
 
-  si_save("Graphics/04b_tx_trends_usaid.svg")
+# 
+   si_save("Images/04b_tx_trends_usaid.png")
+   si_save("Graphics/04b_tx_trends_usaid.svg")
   
-  #data point for context
-  df_tx %>% 
-    filter(period == max(period),
-           indicator == "TX_CURR") %>%
-    select(indicator, value) %>% 
-    mutate(value = clean_number(value, 1))
+  
+  
+  
+
   
