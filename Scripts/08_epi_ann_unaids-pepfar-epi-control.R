65a931a0e6c7d3a297793d7c10352617267d04b8
Karishma Srikanth <krs8rb@virginia.edu>
2023-08-25 17:30:57 -0400

    update for 2023 unaids estimates

--- Scripts/08_epi_ann_unaids-pepfar-epi-control.R
+++ Scripts/08_epi_ann_unaids-pepfar-epi-control.R
@@ -1,9 +1,10 @@
 # PROJECT:  agitprop
 # AUTHOR:   A.Chafetz | T.Essam | K. Srikanth USAID
 # PURPOSE:  Epi control trend graphs
+# REF ID:   d565f3f7 
 # LICENSE:  MIT
 # DATE:     2021-12-01
-# UPDATED:  
+# UPDATED:  2023-08-25 update for 2023
 # NOTE:     derived from agitprop/24b_HIV_epi_control_country.R and catch-22/gpm_country_historic_epi-control
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -30,16 +31,18 @@
 
   load_secrets()
 
+ref_id <- "d565f3f7"
+
   authors <- c("Aaron Chafetz", "Tim Essam", "Karishma Srikanth")
   
   #source info & definition of epidemic control
   source <- source_note 
-  date_pulled <- "August 2022"
+  date_pulled <- "August 2023"
   
   note <- str_wrap("HIV epidemic control is the point at which the number number of new HIV infections falls below the number of total deaths to PLHIV", width = 40)
   
   # epi_control <- str_wrap("PEPFAR defines national HIV epidemic control as the point at which the total number of new HIV infections falls below the total number of deaths from all causes among HIV-infected individuals, with both new infections and deaths among HIV-infected individual slowing and declining.", width = 100)
-  epi_control <- "11 PEPFAR countries have reached epidemic control, where new infections falls below total deaths and total deaths are declining. "
+  epi_control <- "20 PEPFAR countries have reached epidemic control, where new infections falls below total deaths and total deaths are declining. "
   
   plot_title <- "STEADY DECLINE IN THE NUMBER OF <span style= 'color:#2057a7;'> NEW HIV INFECTIONS</span> AND <span style = 'color:#c43d4d;'> TOTAL DEATHS TO PLHIV </span> SINCE THE EARLY 2000s"
   
@@ -74,25 +77,36 @@
     filter(
       #stat == "est",
            age == "All",
-           indicator %in% c("Number New HIV Infections")) %>%
+           sex == "All",
+           indicator %in% c("Number New HIV Infections", "Total deaths to HIV Population")) %>%
     # semi_join(pepfar_country_list, by = c("iso" = "countryname_iso")) %>%
     select(year, country, indicator, estimate) %>%
     arrange(country, indicator, year)  
+
   
-  #grab total deaths
-  total_deaths <- df_deaths %>% 
-    #select(-c(iso2, geo_level)) %>% 
-    filter(age == "all",
-           sex == "all") %>% 
-    select(c(country, year, indicator, estimate)) %>% 
-    spread(indicator, estimate) %>% 
-    janitor::clean_names() %>% 
-    rename(total_deaths = number_total_deaths_hiv_pop)
+  df_epi %>% 
+    filter(
+      year == max(year),
+      age == "All",
+      sex == "All",
+      indicator %in% c("Number New HIV Infections")) %>% 
+    count(epi_control)
+  
+  # #grab total deaths
+  # total_deaths <- df_deaths %>% 
+  #   #select(-c(iso2, geo_level)) %>% 
+  #   filter(age == "all",
+  #          sex == "all") %>% 
+  #   select(c(country, year, indicator, estimate)) %>% 
+  #   spread(indicator, estimate) %>% 
+  #   janitor::clean_names() %>% 
+  #   rename(total_deaths = number_total_deaths_hiv_pop)
 
   df_epi_pepfar <- df_epi_pepfar %>% 
-    mutate(indicator = word(indicator, -1) %>% tolower) %>%
-    pivot_wider(names_from = "indicator", values_from = "estimate") %>%
-    left_join(total_deaths, by = c("year", "country")) %>% 
+        pivot_wider(names_from = "indicator", values_from = "estimate") %>%
+    rename(infections = `Number New HIV Infections`,
+           total_deaths = `Total deaths to HIV Population`) %>% 
+   # left_join(total_deaths, by = c("year", "country")) %>% 
     group_by(country) %>% 
     mutate(declining_deaths = total_deaths - lag(total_deaths, order_by = year) <= 0) %>% 
     ungroup() %>% 
@@ -158,7 +172,24 @@
     si_style_ygrid() +
     theme(axis.text.y = element_markdown())
   
-  v_c <- df_viz_cntry %>% 
+  
+  high_vol_cntry <- df_viz_cntry %>% 
+    filter(year == max(year),
+           indicator == "infections",
+           value > 5000) %>% 
+    count(country) %>% 
+    pull(country)
+  
+  
+  low_vol_cntry <- df_viz_cntry %>% 
+    filter(year == max(year),
+           indicator == "infections",
+           value <= 5000) %>% 
+    count(country) %>% 
+    pull(country)
+  
+  v_c_high <- df_viz_cntry %>% 
+    filter(country %in% c(high_vol_cntry)) %>% 
     ggplot(aes(year, value_mod, group = indicator, fill = fill_color, color = fill_color)) +
     geom_blank(aes(y = max_plot_pt)) +
     geom_blank(aes(y = -max_plot_pt)) +
@@ -170,7 +201,32 @@
     geom_text(aes(label = val_lab), na.rm = TRUE,
               hjust = -0.3,
               family = "Source Sans Pro Light") +
-    facet_wrap(~country, nrow = 4, scales = "free_y") +
+    facet_wrap(~country, nrow = 2, scales = "free_y") +
+    scale_y_continuous(labels = ~ label_number_si()(abs(.))) +
+    scale_x_continuous(breaks = seq(1990, 2025, 10)) +
+    scale_fill_identity(aesthetics = c("fill", "color")) +
+    labs(x = NULL, y = NULL) +
+    coord_cartesian(expand = T, clip = "off") +
+    si_style_ygrid() +
+    theme(axis.text.y = element_markdown(),
+          axis.text.x = element_blank(),
+          panel.spacing.x = unit(20, "pt"),
+          panel.spacing.y = unit(0, "pt"))
+  
+  v_c_low <- df_viz_cntry %>% 
+    filter(country %in% c(low_vol_cntry)) %>% 
+    ggplot(aes(year, value_mod, group = indicator, fill = fill_color, color = fill_color)) +
+    geom_blank(aes(y = 50000)) +
+    geom_blank(aes(y = -50000)) +
+    geom_area(alpha = .25) +
+    geom_hline(yintercept = 0, color = grey80k) +
+    geom_line() +
+    geom_point(aes(y = lab_pt), na.rm = TRUE,
+               shape = 21, color = "white", size = 3) +
+    geom_text(aes(label = val_lab), na.rm = TRUE,
+              hjust = -0.3,
+              family = "Source Sans Pro Light") +
+    facet_wrap(~country, nrow = 2, scales = "free_y") +
     scale_y_continuous(labels = ~ label_number_si()(abs(.))) +
     scale_x_continuous(breaks = seq(1990, 2025, 10)) +
     scale_fill_identity(aesthetics = c("fill", "color")) +
@@ -181,13 +237,13 @@
           panel.spacing.x = unit(20, "pt"),
           panel.spacing.y = unit(0, "pt"))
   
-  
-  v_p + v_c +  
+  #patchwork
+  v_p + (v_c_high/v_c_low) +  
+    plot_layout(widths = c(0.75, 1)) +
     plot_annotation(title = plot_title,
                     subtitle = epi_control,
-                    caption = glue("Source: {source} [{date_pulled}]
-                        SI analytics: {paste(authors, collapse = '/')}
-                     US Agency for International Development"),
+                    caption = glue("Source: {source_note} [{date_pulled}]
+                        SI analytics | Ref id: {ref_id}"),
                     theme = si_style_ygrid()) &
     theme(axis.text.y = element_markdown(),
           panel.spacing.x = unit(20, "pt"),
@@ -195,7 +251,8 @@
           plot.title = element_markdown())
 
   
-  si_save("Graphics/08_epi_ann_unaids-pepfar-epi-control.svg")
+  si_save("Graphics/08_epi_ann_unaids-pepfar-epi-control_update2022.svg")
+  si_save("Graphics/08_epi_ann_unaids-pepfar-epi-control_update2022.png")
   
   
  
